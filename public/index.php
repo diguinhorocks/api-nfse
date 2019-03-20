b8b42a1041726d5be884d6cd436a656b2ae3ba98
Rodrigo Santiago <rsantiago897@gmail.com>
2019-03-20 13:10:00 -0300

    rotas

--- public/index.php
+++ public/index.php
@@ -6,4 +6,9 @@ use Respect\Rest\Router;
 
 $r3 = new Router;
 
-$r3->get('/nf/generate', array('Nf\Controller\Nf', 'getNfInfo'));
+$r3->post('/nfse/validate', array('NFSe\Controller\NFSeController', 'validateNFSeInfo'));
+
+$r3->post('/nfse/send', array('NFSe\Controller\NFSeController', 'sendNFSeInfo'));
+$r3->post('/nfse/read-batch/*', array('NFSe\Controller\NFSeController', 'readBatchNFSeInfo'));
+$r3->post('/nfse/read/*', array('NFSe\Controller\NFSeController', 'readNFSeInfo'));
+$r3->post('/nfse/cancel/*', array('NFSe\Controller\NFSeController', 'cancelNFSe'));
